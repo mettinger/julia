bc5b8776a7d8d81af2f89492b967ebfe14968a69
mettinger <jettinger35@gmail.com>
2024-04-15 14:58:23 -0700

    comment

--- categories.jl
+++ categories.jl
@@ -2,25 +2,46 @@ using Metatheory
 using Metatheory.EGraphs
 using Metatheory.Library
 
+using Debugger
+
 mutable struct Category
     src::Dict
     target::Dict
     relations
 end
 
+function checkId(morphism)
+    if occursin("id", String(morphism))
+        return true
+    else
+        return false
+    end
+end
+
+function addIdentityMorphism(src, target)
+    objects = union(Set(values(src)), Set(values(target)))
+    for thisObject in objects
+        src[Symbol("id" * string(thisObject))] = thisObject
+        target[Symbol("id" * string(thisObject))] = thisObject
+    end
+    return src,target
+end
+
 # Initialize a category and test it 
 function categoryInit(src::Dict, target::Dict, relations)
 
-    identity = @theory f begin
-        1 ∘ f == f 
-        f ∘ 1 == f
+    identity = @theory begin
+        ~g::checkId ∘ ~f --> ~f 
+        ~f ∘ ~g::checkId--> ~f
     end
-    
+
+    relations = relations ∪ identity
+    src, target = addIdentityMorphism(src, target)
+
     cat = Category(src, target, relations);
     if checkComposability(cat, true)
         if checkComposition(cat, true)
             if checkAssociativity(cat, true)
-                cat.relations = cat.relations ∪ identity
                 return cat
             end
         end
@@ -31,6 +52,10 @@ end;
 function checkComposability(c::Category, debug::Bool=false)
     for thisEquality in c.relations
         second, first = thisEquality.left.args 
+        if typeof(second) == PatTerm 
+            second = (second.args[1], second.args[2])
+            first = (first.args[1], first.args[2])
+        end
         
         if c.src[second] != c.target[first]
             if debug
@@ -90,7 +115,7 @@ end;
 #Check that the composition of any two arrows with matching sources and targets has a name by brute force
 function checkComposition(c::Category, debug::Bool=false) :: Bool
     
-    morphisms = keys(c.src)
+    morphisms = [morph for morph in keys(c.src) if !occursin("id", string(morph))]
     leftSides = Set([string(thisEquality.left) for thisEquality in c.relations])
 
     for i in morphisms
@@ -111,6 +136,38 @@ function checkComposition(c::Category, debug::Bool=false) :: Bool
     return true
 end;
 
+function productCategory(c::Category, d::Category)
+    src = srcProduct(c, d)
+    target = targetProduct(c, d)
+    relations = relationsProduct(c, d)
+    productCat = categoryInit(src, target, relations)
+    return productCat
+end;
+
+function srcProduct(c::Category, d::Category)
+    srcKeys = [(i,j) for i in keys(c.src) for j in keys(d.src)]
+    srcValues = [(i,j) for i in values(c.src) for j in values(d.src)]
+    return Dict(zip(srcKeys, srcValues))
+end
+
+function targetProduct(c::Category, d::Category)
+    targetKeys = [(i,j) for i in keys(c.target) for j in keys(d.target)]
+    targetValues = [(i,j) for i in values(c.target) for j in values(d.target)]
+    return Dict(zip(targetKeys, targetValues))
+end
+
+function relationsProduct(c::Category, d::Category)
+    relations = Vector{EqualityRule}([])
+    for cRelation in c.relations
+        for dRelation in d.relations
+            temp = 5
+            thisRelation = @rule ($(cRelation.left.args[1]), $(dRelation.left.args[1])) ∘ ($(cRelation.left.args[2]), $(dRelation.left.args[2]))  == ($(cRelation.right), $(dRelation.right))
+            push!(relations, thisRelation)
+        end
+    end
+    return relations
+end;
+
 src = Dict(:f => 0, :g => 1, :h => 2, :i => 0, :k => 1, :m => 0, :n => 0)
 target = Dict(:f => 1, :g => 2, :h => 3, :i => 2, :k => 3, :m => 3, :n => 3)
 
@@ -121,43 +178,18 @@ relations = @theory begin
     :k ∘ :f == :m 
 end
 
-myCat = categoryInit(src, target, relations);
-
-function productCategory(c::Category, d::Category)
-    src = srcProduct(c, d)
-    target = targetProduct(c, d)
-    relations = relationsProduct(c, d)
-    productCat = categoryInit(src, target, relations)
-    return productCat
-end;
-
-function srcProduct(c::Category, d::Category)
-    srcKeys = [(i, j) for i in keys(c.src) for j in keys(d.src)]
-    srcValues = [(i, j) for i in values(c.src) for j in values(d.src)]
-    return Dict(zip(srcKeys, srcValues))
-end
-
-function targetProduct(c::Category, d::Category)
-    targetKeys = [(i, j) for i in keys(c.target) for j in keys(d.target)]
-    targetValues = [(i, j) for i in values(c.target) for j in values(d.target)]
-    return Dict(zip(targetKeys, targetValues))
-end
-
-function relationsProduct(c::Category, d::Category)
-    relations = Vector{EqualityRule}([])
-    for cRelation in c.relations
-        for dRelation in d.relations
-            thisRelation = @rule ($(cRelation.left.args[1]), $(dRelation.left.args[1])) ∘ ($(cRelation.left.args[2]), $(dRelation.left.args[2]))  == ($(cRelation.right), $(dRelation.right))
-            push!(relations, thisRelation)
-        end
-    end
-    return relations
-end;
-
+temp = 5
 cat1 = categoryInit(src, target, relations);
 cat2 = categoryInit(src, target, relations);
 
-cat1CrossCat2 = productCategory(cat1, cat2)
+#cat1CrossCat2 = productCategory(cat1, cat2)
 
-using NBInclude
-nbexport("categories.jl", "categories.ipynb")
+temp = 5
+
+
+#=
+    identity = @theory begin
+        ~g::(m -> if occursin("id", String(morphism)) return true else return false end) ∘ ~f --> ~f 
+        ~f ∘ ~g::(m -> if occursin("id", String(morphism)) return true else return false end) --> ~f
+    end
+    =#
